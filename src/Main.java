a6b862606fc50f90d2b1929f1d2bab8d886d6466
David Pervakov <pervakovdavid@gmail.com>
2021-03-22 19:33:09 +0300

    Novii zombi

---
+++ src/Main.java
@@ -0,0 +1,45 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+    }
+
+    public static class Monster {
+
+
+
+        public Monster(String name, int i) {
+        }
+
+        public class Zombie extends Monster {
+
+            public Zombie(String name, int damage) {
+                super(name + " the Zombie", 5);
+            }
+
+
+            public void growl(){
+                System.out.print("Raaaauuughhhh");
+                super.growl();
+            }
+
+
+            public void attack(){
+                super.attack();
+                growl();
+            }
+
+
+        }
+        private void growl() {
+        }
+
+        private void attack() {
+        }
+
+    }
+     
+
+
+}
+
